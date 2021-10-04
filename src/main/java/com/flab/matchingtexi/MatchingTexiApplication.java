13e2fcd4d436d39f31fd53d55d10b03bf3bdecbe
이태윤 <tyleeg0901@gmail.com>
2021-10-04 18:42:28 +0900

    initial commit

---
+++ src/main/java/com/flab/matchingtexi/MatchingTexiApplication.java
@@ -0,0 +1,12 @@
+package com.flab.matchingtexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchingTexiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchingTexiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/flab/matchingtexi/MatchingTexiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.matchingtexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchingTexiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
