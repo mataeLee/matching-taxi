c2a6403a7f7d11f45d9723ed35861f416a5ce4e1
이태윤 <tyleeg0901@gmail.com>
2021-11-09 00:05:26 +0900

    feat : remote message dao layer
    - create/subscribe topic method
    - delete/unsubscribe topic method
    - read topic method

---
+++ src/main/java/com/flab/matchingtaxi/repo/redis/RemoteMessageRepository.java
@@ -0,0 +1,73 @@
+package com.flab.matchingtaxi.repo.redis;
+
+import com.flab.matchingtaxi.service.redis.RemoteMessageSubscriber;
+import com.flab.matchingtaxi.std.RedisStandard;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.stereotype.Repository;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
+@Slf4j
+public class RemoteMessageRepository {
+
+    @Autowired
+    private RedisMessageListenerContainer redisMessageListenerContainer;
+    @Autowired
+    private RemoteMessageSubscriber remoteMessageSubscriber;
+
+    // redis
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+    private HashOperations<String, String, String> hashOperations;
+
+    // topic hash table
+    private Map<String, ChannelTopic> topics;
+
+    @PostConstruct
+    private void init(){
+        hashOperations = redisTemplate.opsForHash();
+        topics = new HashMap<>();
+    }
+
+    // 토픽 생성, 구독
+    public ChannelTopic createTopic(String name){
+        if(!hashOperations.hasKey(RedisStandard.HASHOPS_KEY_CHANNEL, name)){
+            hashOperations.put(RedisStandard.HASHOPS_KEY_CHANNEL, name, name);
+        }
+        String key = hashOperations.get(RedisStandard.HASHOPS_KEY_CHANNEL, name);
+        ChannelTopic topic;
+        if(topics.containsKey(key)){
+            topic = topics.get(key);
+        }
+        else {
+            topic = new ChannelTopic(key);
+            topics.put(key, topic);
+        }
+        redisMessageListenerContainer.addMessageListener(remoteMessageSubscriber, topic);
+        log.info("create topic successed : " + topic.toString());
+        return topic;
+    }
+
+    // 토픽 제거
+    public boolean deleteTopic(String name){
+        ChannelTopic topic = topics.get(name);
+        if(topic == null) return false;
+        topics.remove(name, topic);
+        redisMessageListenerContainer.removeMessageListener(remoteMessageSubscriber, topic);
+        return true;
+    }
+
+    // 토픽 조회
+    public ChannelTopic getTopic(String name){
+        String val = hashOperations.get(RedisStandard.HASHOPS_KEY_CHANNEL, name);
+        return topics.get(val);
+    }
+}
