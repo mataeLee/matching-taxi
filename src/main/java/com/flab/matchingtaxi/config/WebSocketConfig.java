208306906fc9ba0a4b4a42aec6649d3ef0d88df8
이태윤 <tyleeg0901@gmail.com>
2021-11-25 17:43:15 +0900

    fix : fix websocket config
    - remove with sockjs config

--- src/main/java/com/flab/matchingtaxi/config/WebSocketConfig.java
+++ src/main/java/com/flab/matchingtaxi/config/WebSocketConfig.java
@@ -3,9 +3,7 @@ package com.flab.matchingtaxi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -19,8 +17,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/ws")
+//                .setAllowedOrigins("*")
+//                .withSockJS();
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
-                .withSockJS();
+                .setAllowedOrigins("*");
     }
 }
